a6472c05dc275088e786198223c6629105dbb099
PavelReiman <PavelReiman@yandex.ru>
2023-11-15 08:32:02 +0300

    Update Calcul.js

--- Six_lessons/Calcul.js
+++ Six_lessons/Calcul.js
@@ -1,96 +1,35 @@
-class Product {
-    constructor(priceOption, quantity, souce, bag) {
-        this.name = "";
-        this.pricePerOne = "";
-        this.quantity = parseFloat(quantity);
-        this.souce = souce;
-        this.bag = bag;
+function calculateTotal() {
+    const productSelect = document.getElementById("productSelect");
+    const selectedProduct =
+      productSelect.options[productSelect.selectedIndex].value;
 
-        if (this.quantity < 1) {
-            throw new Error('Quantity cannot be less than 1');
-        }
-        if(Number.isInteger(this.quantity) === false) {
-            throw new Error('Quantity should be a positive number');
-        }
+    const quantity = parseFloat(document.getElementById("quantity").value);
 
-        let i=0;
-        for(i; i<priceOption.length && priceOption[i]!==' '; i++) {
-            this.name += priceOption[i];
-        }
-        i++;
-        for(i; i<priceOption.length && priceOption[i]!==' ' && priceOption[i]!=='р'; i++) {
-            this.pricePerOne += priceOption[i];
-        }
-        this.pricePerOne = parseFloat(this.pricePerOne);
+    let price = 0;
+    switch (selectedProduct) {
+        case "1":
+        price = 60;
+        break;
+        case "2":
+        price = 170;
+        break;
+        case "3":
+        price = 250;
+        break;
+        case "4":
+        price = 1500;
+        break;
+        case "5":
+        price = 2500;
+        break;
+        case "6":
+        price = 10000;
+        break;
+      default:
+        price = 0;
     }
 
-    getFinalPrice() {
-        if(this.name==="Бургер Конг")
-            return this.pricePerOne * this.quantity
-        if(this.name==="Чикен Фри")
-            return this.pricePerOne * this.quantity
-        if(this.name==="Королевский завтрак")
-            return this.pricePerOne * this.quantity
-        if(this.name==="Королевский обед")
-            return this.pricePerOne * this.quantity
-        if(this.name==="Королевский ужин")
-            return this.pricePerOne * this.quantity
-        return this.pricePerOne * this.quantity + this.bag + this.souce;
-    }
-}
+    const totalCost = price * quantity;
 
-function onClick(event) {
-    event.preventDefault();
-    let productOption = document.querySelector("select[name=myselect] option:checked").text;
-    let quantity = document.getElementById('quantity').value;
-
-    let souce = Number(document.querySelector("input[name=myradio]:checked") ? document.querySelector("input[name=myradio]:checked").value : 0);
-
-    let bag = document.querySelector("#mycheckbox input[type=checkbox]").checked ? Number(document.querySelector("#mycheckbox input[type=checkbox]").value) : 0;
-
-    try {
-        let product = new Product(productOption, quantity, Number(souce), Number(bag));
-        document.getElementById('finalPrice').textContent = 'Final price: ' + product.getFinalPrice();
-    } catch (error) {
-        console.error(error.message);
-        alert(error.message);
-    }
-}
-
-window.addEventListener('DOMContentLoaded', function (event) {
-    let s = document.getElementsByName("myselect")[0];
-    let c = document.querySelector("#mycheckbox input[type=checkbox]");
-    document.getElementById('button').addEventListener('click', onClick);
-
-    s.addEventListener("change", function (event) {
-        let select = event.target;
-        let radios = document.getElementById("myradios");
-        console.log(select.value);
-
-        if (select.value === "6") {
-            radios.style.display = "none";
-            c.parentNode.style.display = "none";
-        } else if(select.value === "5") {
-            radios.style.display = "none";
-            c.parentNode.style.display = "block";
-        } else if(select.value === "4") {
-            radios.style.display = "none";
-            c.parentNode.style.display = "block";
-        }else if(select.value === "1") {
-            radios.style.display = "none";
-            c.parentNode.style.display = "block";
-        }
-        else {
-            radios.style.display = "block";
-            c.parentNode.style.display = "block";
-        }
-    });
-
-    let r = document.querySelectorAll(".myradios input[type=radio]");
-    r.forEach(function (radio) {
-        radio.addEventListener("change", function (event) {
-            let r = event.target;
-            console.log(r.value);
-        });
-    });
-});
+    document.getElementById("totalCost").textContent = totalCost;
+  }
