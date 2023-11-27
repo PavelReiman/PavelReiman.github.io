37d78b9b8517a21e45f51b724484f76abb9a2cbd
PavelReiman <PavelReiman@yandex.ru>
2023-11-27 12:10:01 +0300

    Update Slider.js

--- Sevan_lessons/Slider.js
+++ Sevan_lessons/Slider.js
@@ -1,21 +1,59 @@
-$(document).ready(function() {
-    $(".slide:first").show();
-    
-    var slideCount = $(".slider").children().length;
-    var currentSlide = 1;
-    
-    function nextSlide() {
-      if (currentSlide < slideCount) {
-        $(".slide").hide();
-        $(".slide:nth-child(" + (currentSlide + 1) + ")").fadeIn();
-        currentSlide++;
-      } else {
-        $(".slide").hide();
-        $(".slide:first").fadeIn();
-        currentSlide = 1;
-      }
+
+// создание слайдера
+function createSlick(number){
+    $('.slider').not('.slick-initialized').slick({
+        arrows:true,
+        dots:true,
+        slidesToShow:number,
+        autoplay:false,
+        speed:1000,
+        autoplaySpeed:800,
+    });
+}
+
+// вычисление количества картинок для показа
+function calculateNumberOfSlidesToShow(){
+    var sliderWidth = $('.slider').width();
+    var slidesToShow = 0;
+    switch (true) {
+        case (sliderWidth < 500):
+            slidesToShow = 1;
+            break;
+        case (sliderWidth < 767):
+            slidesToShow = 2;
+            break;
+        case (sliderWidth < 1199):
+            slidesToShow = 3;
+            break;
+        case (sliderWidth > 1200):
+            slidesToShow = 3;
+            break;
     }
-    
-    setInterval(nextSlide, 3000); // Change slide every 3 seconds
-  });
-  
+
+    return slidesToShow;
+}
+
+// перезагружаем слайдер по изменене размера окна
+function reloadSlick () {
+    $('.slider').slick('unslick');
+    slidesToShow = calculateNumberOfSlidesToShow();
+    createSlick(slidesToShow);
+}
+
+// вызываем каждый раз по ресайзу
+jQuery(window).on("resize", reloadSlick);
+
+
+
+jQuery(document).ready(function () {
+
+    // инициализация слайдера по загрузке страницы
+    if ($('.slider').length) {
+        setTimeout(function () {
+            slidesToShow = calculateNumberOfSlidesToShow();
+            createSlick(slidesToShow);
+        }, 300);
+    }
+
+
+});
