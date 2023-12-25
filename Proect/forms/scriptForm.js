700dd80d3dba886b19def93c99c7a41baab92de5
PavelReiman <PavelReiman@yandex.ru>
2023-12-25 12:13:12 +0300

    Update scriptForm.js

--- Proect/forms/scriptForm.js
+++ Proect/forms/scriptForm.js
@@ -24,7 +24,7 @@ function submitForm() {
     };
 
     // Отправляем данные на Formcarry
-    fetch('https://formcarry.com/s/jnM3Xxtlxy', {
+    fetch('https://formcarry.com/s/EHtMLew1to', {
         method: 'POST',
         headers: {
             'Accept': 'application/json',
